6c4f038a7e1e8ea6ad3b85a8a7356cbeae98c58a
Ashish <ashish.averma12@gmail.com>
2024-04-23 16:26:44 +0530

    fixed the issue of noise in the combined image

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ import numpy as np
 def getStreamUrls():
     urls = os.environ.get('STREAM_URLS')
     if urls is None:
-        return ['rtp://127.0.0.1:6000', 'rtp://127.0.0.1:6001', 'rtp://127.0.0.1:6002']
+        return ['rtp://127.0.0.1:6000', 'rtp://127.0.0.1:6002', 'rtp://127.0.0.1:6004']
     urls = ast.literal_eval(urls)
     return [url.strip() for url in urls]
 
@@ -23,34 +23,35 @@ def releaseStreams(streams: list[cv2.VideoCapture]):
 
 # Scale frame to half it's size
 def scale(frame: cv2.typing.MatLike):
-    return cv2.resize(src=frame, fx=0.1, fy=0.1)
+    return cv2.resize(src=frame, dsize=None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
 
 
 # Scales frames
 def scaleFrames(frames: list[cv2.typing.MatLike]):
-    return [None if not frame else scale(frame) for frame in frames]
+    return [frame if frame is None else scale(frame) for frame in frames]
 
 
 # Returns default dimensions or the dimensions from one of the images that is passed in the array
 def getDimension(frames: list[cv2.typing.MatLike]):
-    filteredFrames = filter(lambda frame: frame is not None, frames)
-    if len(frame) > 0:
+    filteredFrames = list(filter(lambda frame: frame is not None, frames))
+    if len(filteredFrames) > 0:
         frame: cv2.typing.MatLike = filteredFrames[0]
-        return frame.shape()
+        return frame.shape
     return (360, 240, 3)
 
 
 # Sanitizes frames if a frame is not present return a black image for the same
 def getSanitizedFrames(frames: list[cv2.typing.MatLike]):
     dims = getDimension(frames)
-    return [np.zeros(dims) if not frame else frame for frame in frames]    
+    return [np.zeros(dims) if frame is None else frame for frame in frames]    
 
 
 # Tries to get the stream
 def getStream(url: str):
+    print(f'[Info] -- Getting stream: {url}')
     try:
         stream = cv2.VideoCapture(url)
-        return stream
+        return stream if stream.isOpened() else None
     except:
         return None
 
@@ -65,16 +66,26 @@ def getFrameFromStream(stream: cv2.VideoCapture):
             return None
         return frame
 
+def stackVertically(frameA: cv2.typing.MatLike, frameB=None):
+    if frameB is None:
+        return frameA
+    return np.row_stack((frameB, frameA))
 
 def concatenate(frames: list[cv2.typing.MatLike]):
-    h_stacks = []
+    h_stacks = None
     for i in range(0, len(frames), 2):
         if i + 1 < len(frames):
-            h_stacks.push(np.concatenate((frames[i], frames[i + 1]), axis=0))
+            h_stacks = stackVertically(
+                np.column_stack((frames[i], frames[i + 1])),
+                h_stacks
+            )
         else:
-            h_stacks.push(np.concatenate((frames[i], np.zeros(frames[i].shape())), axis=0))
+            h_stacks = stackVertically(
+                np.column_stack((frames[i], 255 * np.ones(frames[i].shape, dtype=np.uint8))),
+                h_stacks
+            )
 
-    return np.concatenate(h_stacks)
+    return h_stacks
 
 
 # Driver function
@@ -85,16 +96,16 @@ def main():
         # Get Streams if stream is not present
         for url in streamUrls:
             stream = streamMap[url]
-            if not stream:
+            if stream is None:
                 stream = getStream(url)
                 streamMap[url] = stream
 
         frames = [getFrameFromStream(stream) for stream in streamMap.values()]
-        scaledFrames = scaledFrames(frames)
+        scaledFrames = scaleFrames(frames)
         sanitizedFrames = getSanitizedFrames(scaledFrames)
         displayframe = concatenate(sanitizedFrames)
         cv2.imshow('Mixer', displayframe)
-        if cv2.waitKey(0) & 0xFF == ord('q'):
+        if cv2.waitKey(25) & 0xFF == ord('q'):
             releaseStreams(streamMap.values())
             break
         
